1a90b04702386b8258e1aa1314aab5d37258a7a0
Alessandro Carucci <alessandrocarucci.ac@gmail.com>
2018-01-30 17:03:50 +0000

    SmartTARGA JS + Lcd + Led + Buzzer

--- SmartTARGA Prototyping/JS/README.md
+++ SmartTARGA Prototyping/JS/README.md
@@ -1,6 +1,7 @@
 Uso delle librerie UPM di Intel:
 - Grove UltraSonic Sensor --> npm install jsupm_groveultrasonic
 - Grove Piezo Vibration Sensor --> npm install jsupm_ldt0028
+- Grove LCD RGB BackLight Sensor --> npm install jsupm_ic2lcd
 
 Uso del Framework NodeJS Johnny-Five 
 - Installazione Framework --> npm install johnny-five
--- SmartTARGA Prototyping/JS/SmartTARGA_piezo_ultra.js
+++ SmartTARGA Prototyping/JS/SmartTARGA_piezo_ultra.js
@@ -1,5 +1,6 @@
 var five = require("johnny-five");                        // inclusione librerie framework JS Johnny-Five
 var Edison = require("edison-io");                        // inclusione librerie board Intel Edison per Johnny-Five
+var LCD = require('jsupm_i2clcd');                        // inclusione libreria Grove LCD RGB BackLight
 var ultrasensor = require('jsupm_groveultrasonic');       // inclusione libreria UPM Grove UltraSonic Sensor
 var piezosensor = require('jsupm_ldt0028');               // inclusione libreria UPM Grove Piezo Vibration Sensor
 
@@ -7,14 +8,38 @@ var board = new five.Board({
     io: new Edison()
 });
 
-var MyUltrasensor = new ultrasensor.GroveUltraSonic(7);
-var MyPiezosensor = new piezosensor.LDT0028(4);
+var MyLcd = new LCD.Jhd1313m1(0, 0x3E, 0x62);
+var MyUltrasensor = new ultrasensor.GroveUltraSonic(5);
+var MyPiezosensor = new piezosensor.LDT0028(3);
+
+MyLcd.setCursor(0,0);
+MyLcd.setColor(0,0,0);
 
 board.on("ready", function(){
-    this.pinMode(4, five.Pin.INPUT);
-    this.digitalRead(4, function(value){
+    this.pinMode(3, five.Pin.INPUT);
+    this.pinMode(2, five.Pin.OUTPUT);
+    this.pinMode(4, five.Pin.OUTPUT);
+    this.pinMode(8, five.Pin.OUTPUT);
+    this.digitalRead(3, function(value){
+    var positionCounter;
+        for (positionCounter = 0; positionCounter <29; positionCounter++) {
         if (value===1){
-            console.log("Probabile Collisione");
+            console.log("Collisione");
+        this.digitalWrite(2,1);
+        this.digitalWrite(4,0);
+        this.digitalWrite(8,1);
+        MyLcd.setCursor(0,0);
+        MyLcd.setColor(255,0,0);
+        MyLcd.write('Collisione');
+        }
+        else {
+        this.digitalWrite(2,0);
+        this.digitalWrite(4,1);
+        this.digitalWrite(8,0);
+        MyLcd.clear();
+        MyLcd.setCursor(0,0);
+        MyLcd.setColor(0,0,0);
+        }
         }
     });
 });
@@ -22,14 +47,23 @@ var myInterval = setInterval(function()
 {
   var travelTime = MyUltrasensor.getDistance();
   if (travelTime > 0) {
-    var distance = (travelTime / 29 / 2).toFixed(3);
+    var distance = (travelTime / 29 / 2).toFixed(2);
     var distancefloat = parseInt(distance);
     console.log("Distanza: " + distancefloat + " cm");
+    MyLcd.setCursor(0,0);
+    MyLcd.setColor(0,0,255);
+    MyLcd.write("Distanza:" + distancefloat + " cm");
+    var positionCounter; 
+    for (positionCounter = 0; positionCounter < 29; positionCounter++) {
     if (distancefloat<=10){
     console.log("Avvio camera");
-      }
-  }
-}, 100);
+    MyLcd.setCursor(0,0);
+    MyLcd.setColor(255,0,0);
+    MyLcd.write('  Avvio camera  ');
+    } 
+    }
+    }
+},100);
 
 process.on('SIGINT', function()
 {
